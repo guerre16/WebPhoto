69c446a0f971ac2941d7b9d0e422297e76fb4cfc
Eduardo Orellana <og17006@ues.edu.sv>
2021-11-05 16:24:59 -0600

    Secciones del sitio y carousel

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- CSS stylesheet -->
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="./CSS/style.css">
+    
+    <link rel="stylesheet" href="./owlcarousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="./owlcarousel/assets/owl.theme.default.css">
+    
     <!--Title-->
     <title>Photo - Arteria Estudio</title>
 </head>
@@ -26,22 +30,22 @@
 
     <div class="container_">
 
-        <!--Seccion 1-->
+        <!-- Seccion 1 -->
         <div class="section_one row">
             <div class="col-lg-7 col-md-6 col-sm-12 col-xs-12 title-container">
                 <div class="title-section">
-                    <h2 class="title-principal">Tu negocio</h2>
-                    <h2 class="title-principal">al alcance de</h2>
-                    <h2 class="title-principal">un click</h2>
+                    <h2 class="title-principal">Capturamos</h2>
+                    <h2 class="title-principal">la esencia de</h2>
+                    <h2 class="title-principal">tu negocio</h2>
                 </div>
                 <div class="description-section">
                     <h4 class="description-p">
-                    Te ayudamos a digitalizar tu empresa transformándola a traves de 
-                    la creación de un nuevo canal de ventas para tu negocio
+                    Deja de sacrificar tu tiempo intentando fotografiar los productos de tu
+                    negocio, nosotros lo hacemos por ti y haz que tu marca se vea increíble.
                     </h4>
                 </div>
                 <div class="btn-section">
-                    <a href="#section_two" id="btn-two">CALCULAR PRECIO DE TU SITIO WEB</a>
+                    <a href="#section_two" id="btn-two">¿CÓMO FUNCIONA?</a>
                 </div>
             </div>
             <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 img-container">
@@ -51,6 +55,224 @@
         <!--Final de seccion 1-->
 
 
+        <!-- Sección 2  Cards -->
+
+        <div class="section_two">
+            <div class="info-section">
+                <div class="title-section-two">
+                    La mejor versión de <div class="title-sub">tu marca</div>
+                </div>
+                <h3 class="subtitle-section">
+                    Dale vida con las mejores imágenes 
+                </h3>
+                <h4 class="description-section">
+                    Tenemos los fotógrafos mas talentosos que harán 
+                    realidad tu visión de marca.
+                </h4>
+            </div>
+            <div class="cards-container">
+                <a href="#" class="card-item" id="card-1">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Alimentos
+                        </h2>
+                        <p class="card-description">
+                            Somos especialistas en crear fotografías que dan hambre.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-2">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            E-commerce
+                        </h2>
+                        <p class="card-description">
+                            Fotografías para mostrar la calidad y detalle de tus productos, ¡las hacemos!
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-3">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Lifestyle
+                        </h2>
+                        <p class="card-description">
+                            Refleja la experiencia de tu marca con fotografías auténticas.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-4">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Retratos
+                        </h2>
+                        <p class="card-description">
+                            Capturamos tu esencia personal o profesional en una toma.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-5">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Indumentaria
+                        </h2>
+                        <p class="card-description">
+                            Destacamos tu producto mostrando sus texturas, acabados y detalles.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-6">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Eventos
+                        </h2>
+                        <p class="card-description">
+                            Capturamos los momentos que hacen especiales tus eventos.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-7">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Accesorios
+                        </h2>
+                        <p class="card-description">
+                            Fotografías para los detalles que hacen la diferencia.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-8">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Mobiliario
+                        </h2>
+                        <p class="card-description">
+                            Transmitimos el confort y estilo de tus productos de manera auténtica.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-9">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Áreas
+                        </h2>
+                        <p class="card-description">
+                            Fotos de otro nivel capturadas con drone.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-10">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Productos
+                        </h2>
+                        <p class="card-description">
+                            Mostramos la mejor cara de tus productos.
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class="card-item" id="card-11">
+                    <div class="card-sup"></div>
+                    <div class="card-inf">
+                        <h2 class="card-title">
+                            Espacios
+                        </h2>
+                        <p class="card-description">
+                            Mostramos la escencia de los espacios con detalles y piezas que lo hacen excepcional.
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- Fin seccion 2 -->
+
+        <!-- Sección 3 -->
+        <div class="section_three">
+            <div class="info-section">
+                <div class="title-section-three">
+                    <h2 class="title-sub">Sin pérdidas</h2> de tiempo
+                </div>
+                <h3 class="description-section-three">
+                    Nos encargamos de organizar todo para tu sesión, tendrás tu 
+                    propio equipo que te acompañará en todo el proceso.
+                </h3>
+            </div>
+            <div class="block-container">
+                <div class="block-item">
+                    <h4 class="block-title">
+                        Account Manager
+                    </h4>
+                    <h4 class="block-description">
+                        Te acompañará en toda la logística para realizar tus fotografías.
+                    </h4>
+                </div>
+                <div class="block-plus">+</div>
+                <div class="block-item">
+                    <h4 class="block-title">
+                        Director de arte
+                    </h4>
+                    <h4 class="block-description">
+                        Se encargará que todo esté perfecto para la captura de las imágenes. 
+                    </h4>
+                </div>
+                <div class="block-plus">+</div>
+                <div class="block-item">
+                    <h4 class="block-title">
+                        Fotógrafo
+                    </h4>
+                    <h4 class="block-description">
+                        Tendrás a un especialista de fotografía que hará capturas increíbles.
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <!-- Fin seccion 3 -->
+
+
+        <!-- Seccion 4 -->
+
+        <div class="section_four">
+            <div class="info-section">
+                <div class="title-section-four">
+                    Resultados <h2 class="title-sub">garantizados</h2>
+                </div>
+                <h3 class="description-section-four">
+                    Nos encargamos de organizar todo para tu sesión, tendrás tu 
+                    propio equipo que te acompañará en todo el proceso.
+                </h3>
+            </div>
+
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="./images/carousel-1.png" alt="">
+                </div>
+                <div class="item">
+                    <img src="./images/carousel-2.png" alt="">
+                </div>
+                <div class="item">
+                    <img src="./images/carousel-3.png" alt="">
+                </div>
+                <div class="item">
+                    <img src="./images/carousel-1.png" alt="">
+                </div>
+            </div>
+
+        </div>
+
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./owlcarousel/owl.carousel.min.js"></script>
+    <script src="./JS/app.js"></script>
 </body>
 </html>
