90e1f96c1b53b699767f9c90892c386b74d5de8d
Eduardo Orellana <og17006@ues.edu.sv>
2021-11-05 10:14:44 -0600

    Agregar cambios css e index

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- CSS stylesheet -->
+    <link rel="stylesheet" href="./CSS/style.css">
+    <!--Title-->
+    <title>Photo - Arteria Estudio</title>
+</head>
+<body>
+
+    <!-- Header -->
+    <header class="header">
+      <div class="header__block">
+        <div class="header__img"></div>
+      </div>
+      <div class="header__boton">
+        <a href="#" id="btn-one">CALCULAR PRECIO</a>
+      </div>
+    </header>
+
+    <div class="container_">
+
+        <!--Seccion 1-->
+        <div class="section_one row">
+            <div class="col-lg-7 col-md-6 col-sm-12 col-xs-12 title-container">
+                <div class="title-section">
+                    <h2 class="title-principal">Tu negocio</h2>
+                    <h2 class="title-principal">al alcance de</h2>
+                    <h2 class="title-principal">un click</h2>
+                </div>
+                <div class="description-section">
+                    <h4 class="description-p">
+                    Te ayudamos a digitalizar tu empresa transformándola a traves de 
+                    la creación de un nuevo canal de ventas para tu negocio
+                    </h4>
+                </div>
+                <div class="btn-section">
+                    <a href="#section_two" id="btn-two">CALCULAR PRECIO DE TU SITIO WEB</a>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 img-container">
+                <div class="img-background"></div>
+            </div>
+        </div>
+        <!--Final de seccion 1-->
+
+
+    </div>
+</body>
+</html>
